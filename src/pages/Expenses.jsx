9f59279cfea6e3d29f2a5ee0b2109cfd2002343f
Stefaan <safwbdev@gmail.com>
2025-02-03 14:21:14 +0800

    Add search function on expenses page

--- src/pages/Expenses.jsx
+++ src/pages/Expenses.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { createExpense, deleteItem, fetchData } from '../helper'
 import { useLoaderData } from 'react-router-dom'
 import Table from '../components/Table'
@@ -43,7 +43,19 @@ export async function actionExpenses({ request }) {
 
 const Expenses = () => {
 
-    const { expenses, budgets } = useLoaderData("expenses")
+    const { expenses, budgets } = useLoaderData("expenses");
+
+    const [query, setQuery] = useState('');
+    const [expenseData, setExpenseData] = useState(undefined);
+
+    useEffect(() => {
+        if (query.length < 0) return;
+
+        let filteredSearch = expenses.filter((exp) => exp.name.toLowerCase().includes(query));
+
+        setExpenseData(filteredSearch)
+    }, [expenses, query])
+
 
     return (
         <div className='grid-lg'>
@@ -53,14 +65,27 @@ const Expenses = () => {
                 </div>
                 <div className="col-span-1 md:col-span-3 order-first md:order-last">
                     <div className='w-full block p-6 bg-white border border-gray-200 rounded-lg shadow-sm hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700 mb-6'>
-                        {expenses && expenses.length > 0 ?
-                            (<div>
-                                <h2 className='mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white'>
-                                    All Expenses <small>({expenses.length}) total</small>
-                                </h2>
-                                <Table expenses={expenses} />
-                            </div>) : (<div className="flex justify-center">
-                                <p className='text-gray-100 text-sm font-bold'>No expenses found</p></div>)}
+                        <div className='grid grid-cols-1 md:grid-cols-2 pb-6'>
+                            <h2 className='mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white'>
+                                {query.length <= 0 && "All"} Expenses <small>({expenseData && expenseData.length > 0 ? expenseData.length : 0}) {query.length <= 0 ? "total" : "found"}</small>
+                            </h2>
+
+                            <input
+                                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-100 leading-tight focus:outline-none focus:shadow-outline"
+                                id="search"
+                                name="search"
+                                placeholder='Search'
+                                required
+                                type="text"
+                                value={query}
+                                onChange={(e) => setQuery(e.target.value)}
+                            />
+                        </div>
+                        {expenseData && expenseData.length > 0 ? (<Table expenses={expenseData} />) : (
+                            <div className="flex justify-center">
+                                <p className='text-gray-100 text-sm font-bold'>No expenses found</p>
+                            </div>)
+                        }
                     </div>
                 </div>
             </div>
