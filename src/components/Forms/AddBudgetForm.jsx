3ee54b1f206f010a1fe9503050f69ce402543e8b
Stefaan <safwbdev@gmail.com>
2025-02-06 10:14:04 +0800

    responsiveness for add budget form

--- src/components/Forms/AddBudgetForm.jsx
+++ src/components/Forms/AddBudgetForm.jsx
@@ -1,74 +1,100 @@
-import React, { useEffect, useRef } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import { Form, useFetcher } from 'react-router-dom'
 import { NEW_BUDGET } from '../../routes';
+import { FaPlus } from "react-icons/fa";
+const AddBudgetForm = ({ isBudgetEmpty = false, isHome = false }) => {
+    console.log(isHome);
 
-const AddBudgetForm = ({ isBudgetEmpty = false }) => {
     const fetcher = useFetcher();
-    const isSubmitting = fetcher.state === "submitting"
-    const isLoading = fetcher.state === "loading"
+    const isSubmitting = fetcher.state === "submitting";
+
+    const [openFormWindow, setOpenFormWindow] = useState(false)
+    const isLoading = fetcher.state === "loading";
 
     const formRef = useRef();
     const focusRef = useRef();
 
     useEffect(() => {
-
         if (isLoading) {
             formRef.current.reset()
             focusRef.current.focus()
+            setOpenFormWindow(false)
         }
     }, [isLoading])
 
+    useEffect(() => {
+        if (!openFormWindow) {
+            formRef.current.reset()
+        }
+    }, [openFormWindow])
+
     return (
-        <div className="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow-sm hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700 mb-6">
-            <fetcher.Form method='post' ref={formRef}>
-                <h3 className='mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white'> {isBudgetEmpty ? "Create a budget to get started" : "New Budget"}</h3>
-                <div className="mb-4">
-                    <label
-                        htmlFor="budget"
-                        className="block text-gray-100 text-sm font-bold mb-2">
-                        Budget Name
-                    </label>
-                    <input
-                        className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-100 leading-tight focus:outline-none focus:shadow-outline"
-                        id="newBudget"
-                        name="newBudget"
-                        placeholder='e.g. Groceries'
-                        ref={focusRef}
-                        required
-                        type="text"
-                    />
+        <>
+            <button
+                className={`bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full focus:outline-none focus:shadow-outline block md:hidden ${isHome ? 'w-full' : 'floatingBtn'}`}
+                onClick={() => setOpenFormWindow(true)}
+
+            >{isHome ? 'Add New Budget' : <FaPlus className="w-5 h-5 my-2 text-gray-500 dark:text-gray-100 group-hover:text-blue-600 dark:group-hover:text-blue-500" />}</button>
+            <div className={`md:block ${openFormWindow ? 'formBackdrop' : 'hidden'}`}>
+                <div className="block w-full max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow-sm hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700 mb-6">
+                    <fetcher.Form method='post' ref={formRef}>
+                        <h3 className='mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white'> {isBudgetEmpty ? "Create a budget to get started" : "New Budget"}</h3>
+                        <div className="mb-4">
+                            <label
+                                htmlFor="budget"
+                                className="block text-gray-100 text-sm font-bold mb-2">
+                                Budget Name
+                            </label>
+                            <input
+                                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-100 leading-tight focus:outline-none focus:shadow-outline"
+                                id="newBudget"
+                                name="newBudget"
+                                placeholder='e.g. Groceries'
+                                ref={focusRef}
+                                required
+                                type="text"
+                            />
+                        </div>
+                        <div className="mb-4">
+                            <label
+                                className="block text-gray-100 text-sm font-bold mb-2"
+                                htmlFor="newBudgetAmount" >
+                                Amount
+                            </label>
+                            <input
+                                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-100 leading-tight focus:outline-none focus:shadow-outline"
+                                id="newBudgetAmount"
+                                inputMode='decimal'
+                                name="newBudgetAmount"
+                                placeholder='e.g., $350'
+                                step={0.01}
+                                required
+                                type="number"
+                            />
+                        </div>
+                        <div className={`flex justify${openFormWindow ? '-between' : '-center'}`}>
+                            <input
+                                name="_action"
+                                type="hidden"
+                                value={NEW_BUDGET} />
+                            <button
+                                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                                disabled={isSubmitting}
+                                type="submit">
+                                <span>{isSubmitting ? "Submitting..." : "Create Budget"}</span>
+                            </button>
+                            {openFormWindow && (<button
+                                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                                type='text'
+                                onClick={() => setOpenFormWindow(false)}
+                            >
+                                <span>Cancel</span>
+                            </button>)}
+                        </div>
+                    </fetcher.Form>
                 </div>
-                <div className="mb-4">
-                    <label
-                        className="block text-gray-100 text-sm font-bold mb-2"
-                        htmlFor="newBudgetAmount" >
-                        Amount
-                    </label>
-                    <input
-                        className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-100 leading-tight focus:outline-none focus:shadow-outline"
-                        id="newBudgetAmount"
-                        inputMode='decimal'
-                        name="newBudgetAmount"
-                        placeholder='e.g., $350'
-                        step={0.01}
-                        required
-                        type="number"
-                    />
-                </div>
-                <div className="flex justify-center">
-                    <input
-                        name="_action"
-                        type="hidden"
-                        value={NEW_BUDGET} />
-                    <button
-                        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-                        disabled={isSubmitting}
-                        type="submit">
-                        <span>{isSubmitting ? "Submitting..." : "Create Budget"}</span>
-                    </button>
-                </div>
-            </fetcher.Form>
-        </div>
+            </div >
+        </>
     )
 }
 
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -93,13 +93,13 @@ const Dashboard = () => {
                                 <LineChart expenseData={expenses} budgetData={budgets} />
                             </div> */}
                             <div className='col-span-1 px-6 order-3 md:order-2'>
-                                {budgets && budgets.length > 0 ? (<AddBudgetForm />
+                                {budgets && budgets.length > 0 ? (<AddBudgetForm isHome />
                                 ) : (
                                     <>
                                         <div className="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow-sm hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700 mb-6 flex justify-center items-center flex-col">
                                             <p className='text-gray-100 text-lg font-bold'>Personal budgeting is the secret to financial freedom</p>
                                         </div>
-                                        <AddBudgetForm isBudgetEmpty />
+                                        <AddBudgetForm isBudgetEmpty isHome />
                                     </>
                                 )}
                             </div>
